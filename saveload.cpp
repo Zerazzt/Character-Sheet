8bddad21a1bddd275e9fae9de9b7165e2977706e
Zerazzt <2003isaaclee@gmail.com>
2021-03-12 18:13:08 -0500

    Create file for the save and load functions.

---
+++ saveload.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "app.h"
+#include <fstream>
+
+void CppCLRWinformsProjekt::application::saveCharacter(System::Object^ send, System::EventArgs^ e)
+{
+
+}
+void CppCLRWinformsProjekt::application::loadCharacter(System::Object^ send, System::EventArgs^ e)
+{
+
+}
